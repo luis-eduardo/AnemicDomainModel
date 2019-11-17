4cc384fd6d63d624a5c35e4e847ce749e71a527e
luis.eduardo <luis.e.hernandez.torres@gmail.com>
2019-11-17 01:34:16 -0500

    Add new Dollars type in forgotten places

--- BeforeCore/src/Logic/Mappings/PurchasedMovieMap.cs
+++ BeforeCore/src/Logic/Mappings/PurchasedMovieMap.cs
@@ -9,7 +9,7 @@ namespace Logic.Mappings
         {
             Id(x => x.Id);
 
-            Map(x => x.Price);
+            Map(x => x.Price).CustomType<decimal>().Access.CamelCaseField(Prefix.Underscore);
             Map(x => x.PurchaseDate);
             Map(x => x.ExpirationDate).Nullable();
             Map(x => x.MovieId);
